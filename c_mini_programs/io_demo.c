2a6c0f4540095ecb38fb1d5c8fec4cb123272578
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-08 11:47:30 +0000

    adding user prompt

--- c_mini_programs/io_demo.c
+++ c_mini_programs/io_demo.c
@@ -3,6 +3,7 @@
 	{
 	char f,m,l;
 	int age;
+	printf("Enter your innitials followed by your age:");
 	scanf("%c %c %c %d", &f, &m, &l, &age);
 	printf("My innitials are %c%c%c and my age is %d .\n", f,m,l,age);
 	return (0);	
