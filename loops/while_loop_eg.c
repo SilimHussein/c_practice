2911e2dae52155537f5c0c37f087eb47044e5aed
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-17 13:53:12 +0000

    while loop example

---
+++ loops/while_loop_eg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- while loop, prints values 1-10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("%d \n", i);
+		i++;
+	}
+return (0);
+}
