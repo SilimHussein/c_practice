d5f15aa547f582a1fabc9e4725a605a3fe580760
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-17 09:26:26 +0000

    for loop example

---
+++ loops/README.md
@@ -0,0 +1,6 @@
+This directory contains looping structures for C
+1. for loop
+2. while loop
+3. do while loop
+4. nested loops
+
---
+++ loops/for_loop_eg1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- contains for loops which prints iteration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 5; i++)
+	{
+		printf("%d \n", i);
+	}
+return (0);
+}
