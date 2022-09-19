a8795bd716d9be7def13c24a57d3653755f6d9a5
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-19 08:01:23 +0000

    pointer address example

---
+++ arrays_and_pointers/pointer_address_eg.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- prints the address of a pointer
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int *p;
+
+	printf("The address of 'p': %p \n", &p);
+	return (0);
+}
