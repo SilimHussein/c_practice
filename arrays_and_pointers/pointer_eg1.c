74bd0361f2967ca4c482e0249ad802c73984bd01
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-19 07:54:22 +0000

    pointer size

---
+++ arrays_and_pointers/pointer_eg1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing size in bytes of a pointer
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Size of a pointer: %lu\n", sizeof(p));
+	return (0);
+}
